81626430f765c336b8336c65707d1651cb1c6e55
dlguddn3024 <dlguddn3024@gmail.com>
2024-07-02 21:29:56 +0900

    Refactor: 상속을 통한 확장

---
+++ src/main/java/tobyspring/hellospring/WebApiExRatePaymentService.java
@@ -0,0 +1,28 @@
+package tobyspring.hellospring;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.stream.Collectors;
+
+public class WebApiExRatePaymentService extends PaymentService{
+
+    @Override
+    BigDecimal getExRate(String currency) throws IOException {
+        URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String response = in.lines().collect(Collectors.joining());
+        in.close();
+
+        ObjectMapper mapper = new ObjectMapper();
+        ExRateData data = mapper.readValue(response, ExRateData.class);
+        BigDecimal exRate = data.rates().get("KRW");
+        return exRate;
+    }
+}
